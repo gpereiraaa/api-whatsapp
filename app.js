29ba58e8c29f881415ef423e2393904a7c81ac33
GUSTAVO PEREIRA LEITE DA SILVA <25132694@CFP127ALUNO.LOCAL>
2025-09-24 16:55:39 -0300

    Faltando apenas uma função, e fazer if else das duas ultimas funcoes, e validacao de entrada

---
+++ app.js
@@ -0,0 +1,21 @@
+/***********************************************************************************************
+ * Objetivo: API responsavel em criar endPoints referente ao Whatsapp
+ * Data: 24/09/2025
+ * Autor: Gustavo Pereira
+ * Versão: 1.0
+ * 
+ * Observações: Instalar dependencias para criar a API 
+ *      express     - npm install express --save       -- Instala as dependencias para criar uma API
+ *      cors        - npm install cors --save          -- Instala as dependencias para configurar as permissões de uma API
+ *      body-parser - npm install body-parser --save   -- Instala as dependencias para receber os tipos de dados via POST ou PUT
+ * 
+ *      Quando for baixar em outra maquina usar - npm i - Para baixar as dependencias do node_modules
+ **************************************************************************************************************************************/
+
+// Import das dependencias 
+//const express = require('express')
+//const cors = require('cors')
+//const bodyParser = require('body-parser')
+
+// Import das funções 
+//const dados = require('./modulo/funcoes.js')
---
+++ modulo/funcoes.js
@@ -0,0 +1,129 @@
+/*****************************************************************************************************
+ * Objetivo: Arquivo responsavel pelas funções para criar a API do Whatsapp
+ * Data: 24/09/2025
+ * Autor: Gustavo Pereira
+ * Versão: 1.0
+ **************************************************************************************************/
+
+// Import do arquivo de dados de usuarios
+const dados = require('./contatos.js')
+
+// Mensagem padrão de erro
+const MESSAGE_ERROR = { status: false, statuscode: 500, development: 'Gustavo Pereira' }
+
+// Função que lista todos os usuarios
+const getAllUsers = function () {
+    let message = { status: true, statuscode: 200, development: 'Gustavo Pereira' }
+    message.dados = dados.contatos['whats-users']
+
+    if (message.dados.length > 0)
+        return message
+    else
+        return MESSAGE_ERROR
+
+}
+
+// Função que lista os dados de um usuário especifico 
+const getDataUser = function (numero) {
+    let message = { status: true, statuscode: 200, development: 'Gustavo Pereira', user: [] }
+    dados.contatos['whats-users'].forEach(function (informacoes) {
+        if (informacoes.number === numero) {
+            message.user.id = informacoes.id,
+                message.user.nome = informacoes.account,
+                message.user.apelido = informacoes.nickname,
+                message.user.numero = informacoes.number,
+                message.user.imagem = informacoes['profile-image'],
+                message.user.cor_de_fundo = informacoes.background,
+                message.user.data_criacao_conta = informacoes['created-since'].start,
+                message.user.data_encerramento_conta = informacoes['created-since'].end
+        }
+    })
+
+    if (message.user.nome.length > 0)
+        return message
+    else
+        return MESSAGE_ERROR
+}
+
+// Função que lista os dados dos contatos de um usuario
+const getDataContactUser = function (numero) {
+    let message = { status: true, statuscode: 200, development: 'Gustavo Pereira', contatos: [] }
+
+    dados.contatos['whats-users'].forEach(function (informacoes) {
+        if (informacoes.number === numero) {
+            informacoes.contacts.forEach(contatos => {
+                let contato = {
+                    'nome': contatos.name,
+                    'numero': contatos.number,
+                    'descricao': contatos.description,
+                    'imagem': contatos.image
+                }
+                message.contatos.push(contato)
+            })
+
+        }
+    })
+    if (message.contatos.length > 0)
+        return message
+    else
+        return MESSAGE_ERROR
+}
+
+// Função que lista todas as mensagens de um usuario
+const getAllMessagesUser = function (numero) {
+    let message = { status: true, statuscode: 200, development: 'Gustavo Pereira', user: '', mensagens: [] }
+    dados.contatos['whats-users'].forEach(function (informacoes) {
+        if (informacoes.number === numero) {
+            message.user = informacoes.account
+            informacoes.contacts.forEach(contatos => {
+                contatos.messages.forEach(mensagens => {
+                    let mensagem = {
+                        'contato': contatos.name,
+                        'de': mensagens.sender,
+                        'conteudo': mensagens.content,
+                        'horario': mensagens.time
+                    }
+                    message.mensagens.push(mensagem)
+                })
+            })
+        }
+    })
+    return message
+}
+
+// Função que lista uma conversa de um usuario e um contato
+const getConversationUserContact = function (numberUser, numberContact) {
+    let message = { status: true, statuscode: 200, development: 'Gustavo Pereira', user: '', contato: '', mensagens: [] }
+    dados.contatos['whats-users'].forEach(function (informacoes) {
+        if (informacoes.number === numberUser) {
+            message.user = informacoes.account
+            informacoes.contacts.forEach(contatos => {
+                if(contatos.number === numberContact){
+                    message.contato = contatos.name
+                    contatos.messages.forEach(mensagens => {
+                        let mensagem = {
+                            'de': mensagens.sender,
+                            'conteudo': mensagens.content,
+                            'horario': mensagens.time
+                        }
+                        message.mensagens.push(mensagem)
+                    })
+                }
+
+            })
+        }
+    })
+    return message
+}
+
+
+
+
+console.log(getConversationUserContact('11987876567', '26999999963'))
+
+module.exports = {
+    getAllUsers,
+    getDataUser,
+    getDataContactUser,
+    getAllMessagesUser
+}
